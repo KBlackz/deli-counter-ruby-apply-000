0895a39decbdc184930386ab0b7fd44579ba4558
blessed-panel-9704 <karris.b@gmail.com>
2020-10-14 21:13:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,8 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  position = katz_deli.index(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+  position = katz_deli.length
+  puts "Welcome, #{name}. You are number #{position} in line."
   return name, position 
 end 
 
